84e1238b6a1eaa831f717c62a97775e5fd138d04
pal_user <ace_14r@yahoo.co.in>
2018-05-21 13:32:25 -0600

    upated

---
+++ src/main/java/io/pivotal/tracker/PalTrackerApp.java
@@ -0,0 +1,12 @@
+package io.pivotal.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalTrackerApp {
+
+    public static void main(String[] arin) {
+        SpringApplication.run(PalTrackerApp.class, arin);
+    }
+}
